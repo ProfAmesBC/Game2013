2c95cf0ec5dc1b4fb0476f32debbef7e3da152da
tim <ventarol@bc.edu>
2013-11-06 16:00:30 -0500

    Motion now smooth.

--- src/game/PlayerMotion.java
+++ src/game/PlayerMotion.java
@@ -9,6 +9,7 @@ public class PlayerMotion implements KeyListener {
     private float eyeX, eyeY, eyeZ;
 	private float theta;
     private float step = 2;
+    private boolean wdown, adown, sdown, ddown, qdown, edown;
     
     public PlayerMotion() {
         eyeX = -5; eyeY = 5; eyeZ = 50;
@@ -31,32 +32,49 @@ public class PlayerMotion implements KeyListener {
 	public void keyPressed(KeyEvent e) {
 	    switch (e.getKeyCode()) {
         case KeyEvent.VK_A:
-            eyeX += step * Math.cos(Math.toRadians(theta + 90));
-            eyeZ += step * -Math.sin(Math.toRadians(theta + 90));
+            adown = true;
             break;
         case KeyEvent.VK_D:
-            eyeX += step * Math.cos(Math.toRadians(theta - 90));
-            eyeZ += step * -Math.sin(Math.toRadians(theta - 90));
+            ddown = true;
             break;
         case KeyEvent.VK_S:
-            eyeX -= step * Math.cos(Math.toRadians(theta));
-            eyeZ -= step * -Math.sin(Math.toRadians(theta));
+            sdown = true;
             break;
         case KeyEvent.VK_W:
-            eyeX += step * Math.cos(Math.toRadians(theta));
-            eyeZ += step * -Math.sin(Math.toRadians(theta));
+            wdown = true;
             break;
         case KeyEvent.VK_Q:
-            theta += 3;
+            qdown = true;
             break;
         case KeyEvent.VK_E:
-            theta -= 3;
+            edown = true;
             break;
 	    }
 	}
 
 	@Override
-	public void keyReleased(KeyEvent e) { /* not needed */ }
+	public void keyReleased(KeyEvent e) { 
+		switch (e.getKeyCode()) {
+        case KeyEvent.VK_A:
+            adown = false;
+            break;
+        case KeyEvent.VK_D:
+            ddown = false;
+            break;
+        case KeyEvent.VK_S:
+            sdown = false;
+            break;
+        case KeyEvent.VK_W:
+            wdown = false;
+            break;
+        case KeyEvent.VK_Q:
+            qdown = false;
+            break;
+        case KeyEvent.VK_E:
+            edown = false;
+            break;
+	    }
+	}
 
 	@Override
 	public void keyTyped(KeyEvent e) { /* not needed */ }
@@ -64,9 +82,35 @@ public class PlayerMotion implements KeyListener {
 	public void setLookAt(GL2 gl, GLU glu) {
         gl.glLoadIdentity();
 //      System.out.println(eyeX + "" + eyeY + "" + eyeZ);
+        update();
         glu.gluLookAt(eyeX, eyeY, eyeZ,   // eye location
                 eyeX + Math.cos(Math.toRadians(theta)), eyeY, eyeZ + -Math.sin(Math.toRadians(theta)),   // point to look at (near middle of pyramid)
                  0, 1,  0); // the "up" direction
+	}
+	
+	public void update() {
+		if(adown) {
+			eyeX += step * Math.cos(Math.toRadians(theta + 90));
+        	eyeZ += step * -Math.sin(Math.toRadians(theta + 90));
+		}
+    	if(ddown) {
+    		eyeX += step * Math.cos(Math.toRadians(theta - 90));
+    		eyeZ += step * -Math.sin(Math.toRadians(theta - 90));
+    	}
+    	if(sdown) {
+    		eyeX -= step * Math.cos(Math.toRadians(theta));
+        	eyeZ -= step * -Math.sin(Math.toRadians(theta));
+    	}
+    	if(wdown) {
+    		eyeX += step * Math.cos(Math.toRadians(theta));
+    		eyeZ += step * -Math.sin(Math.toRadians(theta));
+    	}
+    	if(qdown) {
+    		theta += 3;
+    	}
+    	if(edown) {
+    		theta -= 3;
+    	}
 	}
 
 }
