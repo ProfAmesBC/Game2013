b6d300f32d7a3e1949a07d90c43ae340fd1e226c
Nicola <kahale@bc.edu>
2013-11-20 23:48:27 -0500

    added a keylistener for weapons class

--- src/weapons/Weapons.java
+++ src/weapons/Weapons.java
@@ -7,20 +7,15 @@ public class Weapons implements KeyListener{
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+		if(e.getKeyCode() == KeyEvent.VK_SPACE){
+			
+		}
 	}
-
 	@Override
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+	public void keyPressed(KeyEvent e) {		
 	}
-
 	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+	public void keyReleased(KeyEvent e) {		
 	}
 
 }
