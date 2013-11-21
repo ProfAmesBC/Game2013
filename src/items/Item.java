e7ce4963e73a20bc15569da615d539e9fa5cb8b8
Zeus <Zeus@136.167.202.43>
2013-11-21 18:21:16 -0500

    Created packages for inventory and items. Created initial interfaces for Item and PowerUp with comments. Created initial class for Inventory.

---
+++ src/items/Item.java
@@ -0,0 +1,9 @@
+// Items differ from Power-Ups because
+// they are added to the inventory
+// rather than being used immediately
+
+package items;
+
+public interface Item {
+
+}
---
+++ src/items/PowerUp.java
@@ -0,0 +1,8 @@
+// Items are used immediately upon
+// being picked up by the player
+
+package items;
+
+public interface PowerUp {
+
+}
