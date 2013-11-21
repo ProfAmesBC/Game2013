36ab874a1e1ad44830537a4fb891570e32c5b808
Nicola <kahale@bc.edu>
2013-11-20 23:12:29 -0500

    added weapons class with Keylistener

--- src/weapons/Fireball.java
+++
@@ -1,5 +0,0 @@
-package weapons;
-
-public class Fireball {
-
-}
---
+++ src/weapons/Weapons.java
@@ -0,0 +1,26 @@
+package weapons;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Weapons implements KeyListener{
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
