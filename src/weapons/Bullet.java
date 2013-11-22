40a14b631eb42e59ba1d2edee0c4653a47c46410
Nicola <kahale@bc.edu>
2013-11-21 21:24:01 -0500

    Updated both Bullet and Weapons class, weapons class now watches player to know where starting location for bullet When you hit space bar you will make a stand still bullet

--- src/weapons/Bullet.java
+++ src/weapons/Bullet.java
@@ -1,5 +1,22 @@
 package weapons;
 
-public class Bullet {
+
+
+public class Bullet{
+	private float x, y, z, angle;
+	
+	public float getX() {return x;}
+	public float getY() {return y;}
+	public float getZ() {return z;}
+	public float getAngle() {return angle;}
+
+	public Bullet(float x, float y, float z, float angle){
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.angle = angle;
+	}
+
+
 
 }
--- src/weapons/Weapons.java
+++ src/weapons/Weapons.java
@@ -1,27 +1,69 @@
 package weapons;
 
+
+import game.PlayerMotion;
+import game.PlayerMotionWatcher;
+
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
-public class Weapons implements KeyListener{
+import javax.media.opengl.GL2;
+import javax.media.opengl.glu.GLU;
+import javax.media.opengl.glu.GLUquadric;
+
+public class Weapons implements KeyListener, PlayerMotionWatcher{
 	
-	private Bullet[] bulletsList;
+	private ArrayList<Bullet> bulletsList = new ArrayList<Bullet>();
+	private GLUquadric quadric;
+	private float x, y, z, angle;
+	
+	public Weapons(){
+		PlayerMotion.registerPlayerWatcher(this);
+	}
+	
+	public void update(GL2 gl, GLU glu){
+		quadric = glu.gluNewQuadric();
+        glu.gluQuadricDrawStyle(quadric, GLU.GLU_FILL); // GLU_POINT, GLU_LINE, GLU_FILL, GLU_SILHOUETTE
+        glu.gluQuadricNormals  (quadric, GLU.GLU_SMOOTH); // GLU_NONE, GLU_FLAT, or GLU_SMOOTH
+        glu.gluQuadricTexture  (quadric, true);        // false, or true to generate texture coordinates
+		
+		for(Bullet b: bulletsList){
+			gl.glPushMatrix();
+				gl.glTranslatef(b.getX(), b.getY(), b.getZ());
+				glu.gluSphere(quadric, 0.5, 10, 10);
+			gl.glPopMatrix();
+		}
+        
+        
+        
+	}
 	
 	public void shootBullet(){
-		
+		Bullet bullet = new Bullet(x, y, z, angle);
+		bulletsList.add(bullet);
 	}
 	
 	@Override
 	public void keyTyped(KeyEvent e) {
+
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
 		if(e.getKeyCode() == KeyEvent.VK_SPACE){
 			shootBullet();
+			System.out.println("HI");
 		}
 	}
 	@Override
-	public void keyPressed(KeyEvent e) {		
-	}
+	public void keyReleased(KeyEvent e) {}
+
 	@Override
-	public void keyReleased(KeyEvent e) {		
+	public void playerMoved(float x, float y, float z, float angle) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.angle = angle;
 	}
 
 }
