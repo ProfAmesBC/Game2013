f40f6966b313f6c04e802c4cc68871a0c232b005
Nicola <kahale@bc.edu>
2013-11-25 20:28:52 -0500

    updated weapons so projectile is an abstract class that other projectile types can implement

---
+++ src/weapons/Projectile.java
@@ -0,0 +1,36 @@
+package weapons;
+
+import javax.media.opengl.GL2;
+import javax.media.opengl.glu.GLU;
+
+public abstract class Projectile{
+	private float projX, projY, projZ, projAngle;
+	private float speed = 5;
+	private float lifeSpan = 50;
+	public float getLifeSpan() {
+		return lifeSpan;
+	}
+	public float getProjX() {return projX;}
+	public void setProjX(float projX) {this.projX = projX;}
+	public float getProjY() {return projY;}
+	public void setProjY(float projY) {this.projY = projY;}
+	public float getProjZ() {return projZ;}
+	public void setProjZ(float projZ) {	this.projZ = projZ;}
+	public void setProjAngle(float projAngle) {this.projAngle = projAngle;}
+
+	
+	public abstract void draw(GL2 gl, GLU glu);
+	
+	
+	public void updateLife(){
+		lifeSpan--;
+	}
+	
+	public void updatePosition(){
+		projX = (float) (projX + speed*Math.cos(Math.toRadians(projAngle)));
+		projZ = (float) (projZ - speed*Math.sin(Math.toRadians(projAngle)));
+	}
+	
+
+
+}
