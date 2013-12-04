0e335ede40fc8322d519b2e1b9a2d10a6987baa5
cheungdi <cheungdi@bc.edu>
2013-12-04 01:57:26 -0500

    Rabbit: shrank the Rabbit by reducing constants, translation distances, and scaling factors in the draw methods Cat: created drawBody method, began drawWholeBody and drawHead methods Critter: no changes

--- src/catsrabbits/Cat.java
+++ src/catsrabbits/Cat.java
@@ -2,36 +2,77 @@ package catsrabbits;
 // Diana Cheung, CS333 Class of 2013
 
 import javax.media.opengl.GL2;import javax.media.opengl.glu.GLU;
-import game.*;import java.io.File;
+import game.Building;import java.io.File;
 
 public class Cat extends Critter{
+	private static final float BODY_LENGTH=1.8f,HEAD_HEIGHT=.56f,HEAD_DIST=BODY_LENGTH*.7f,
+			EYE_CENTER_DIST=.52f,EYE_HEIGHT=.35f,
+			WHISKER_COLOR=.82f,TOP_WHISKER_LENGTH=1f,TOP_WHISKER_HEIGHT=.5f,MID_WHISKER_LENGTH=1.25f;
+	private int furColor;
+	private float eyeGreen=0;
+	
 	public Cat(float x,float y,float z,float a,float s,float tR, GL2 gl,GLU glu){
 		super(x,y,z,a,s,tR,gl,glu);
 		tRate=.025f;
-        texture=Building.setupTexture(gl, FUR_DIRECTORY+File.separator+"cheungcatfur"+(int)(Math.random()*5)+".jpg");
+		furColor=(int)(Math.random()*5);
+		if(furColor==3)eyeGreen=1;	// green eyes for black fur cats
+        texture=Building.setupTexture(gl, FUR_DIRECTORY+File.separator+"cheungcatfur"+furColor+".jpg");
 	}
 	
 	public void drawWholeBody(GL2 gl, GLU glu){
 		gl.glPushMatrix();
 			gl.glEnable(GL2.GL_TEXTURE_2D);
 			texture.bind(gl);
-			// draw body
+			drawBody(gl,glu);
 			gl.glDisable(GL2.GL_TEXTURE_2D);
-			// TODO
+			
+			gl.glTranslatef(0,HEAD_HEIGHT,-HEAD_DIST);
+			drawHead(gl,glu);
+			// TODO gl.glTranslatef(0,-HEAD_HEIGHT,HEAD_DIST);
+			
 		gl.glPopMatrix();
 	}
 	
 	protected void drawBody(GL2 gl, GLU glu){
-		// TODO Auto-generated method stub
-		
+		gl.glPushMatrix();
+			gl.glScalef(1, 1, BODY_LENGTH);
+			drawTexturedUnitSphere(glu);
+		gl.glPopMatrix();
 	}
 	protected void drawHead(GL2 gl, GLU glu){
-		// TODO Auto-generated method stub
-		
+		gl.glEnable(GL2.GL_TEXTURE_2D);
+		gl.glPushMatrix();
+			gl.glScalef(1.1f,1,.9f);
+			drawSphere(textureQuadric,.8f,glu);
+		gl.glPopMatrix();
+		gl.glDisable(GL2.GL_TEXTURE_2D);
+		gl.glPushMatrix();
+			// draw eyes
+			gl.glColor3f(0,eyeGreen,0);
+			gl.glTranslatef(0,EYE_HEIGHT,-EYE_CENTER_DIST);
+			drawEyes(gl, glu, .27f, .16f);
+			// draw nose
+			gl.glTranslatef(0, -EYE_HEIGHT, -.74f+EYE_CENTER_DIST);
+			drawNose(gl,glu);
+			// draw ears
+			gl.glTranslatef(0, .8f, .74f);
+			
+		gl.glPopMatrix();
 	}
 	protected void drawNose(GL2 gl,GLU glu){
-		// TODO Auto-generated method stub
-		
+		// draw nose
+		gl.glColor3f(1, .77f, 1);
+		drawSphere(quadric, .14f, glu);
+		// draw whiskers
+		gl.glColor3f(WHISKER_COLOR,WHISKER_COLOR,WHISKER_COLOR);
+		gl.glBegin(GL2.GL_LINES);
+			gl.glVertex3f(TOP_WHISKER_LENGTH, TOP_WHISKER_HEIGHT, 0);
+			gl.glVertex3f(-TOP_WHISKER_LENGTH, -TOP_WHISKER_HEIGHT, 0);
+			gl.glVertex3f(MID_WHISKER_LENGTH, 0, 0);
+			gl.glVertex3f(-MID_WHISKER_LENGTH, 0, 0);
+			gl.glVertex3f(TOP_WHISKER_LENGTH, -TOP_WHISKER_HEIGHT, 0);
+			gl.glVertex3f(-TOP_WHISKER_LENGTH, TOP_WHISKER_HEIGHT, 0);
+		gl.glEnd();
 	}
 	protected void drawEar(GL2 gl, GLU glu){
 		// TODO Auto-generated method stub
--- src/catsrabbits/Rabbit.java
+++ src/catsrabbits/Rabbit.java
@@ -5,9 +5,9 @@ import javax.media.opengl.GL2;import javax.media.opengl.glu.GLU;
 import game.*;import java.io.File;
 
 public class Rabbit extends Critter{
-	private static final float BODY_RADIUS=2,BODY_HEIGHT_WIDTH_RATIO=1.7f,EYE_CENTER_DISTANCE=.46f,EYE_CENTER_HEIGHT=.49f,
-			WHISKER_COLOR=.87f,TOP_WHISKER_LENGTH=1,TOP_WHISKER_HEIGHT=.75f,MID_WHISKER_LENGTH=1.25f,
-			FOOT_CENTER_DISTANCE=.71f;
+	private static final float BODY_RADIUS=1,BODY_HEIGHT_WIDTH_RATIO=1.7f,EYE_CENTER_HEIGHT=.245f,EYE_ZDIST=-.355f,
+			WHISKER_COLOR=.87f,TOP_WHISKER_LENGTH=1,TOP_WHISKER_HEIGHT=.5f,MID_WHISKER_LENGTH=1.25f,
+			FOOT_CENTER_DISTANCE=.355f;
 	
 	public Rabbit(float x,float y,float z,float a,float s,float tR, GL2 gl,GLU glu){
 		super(x,y,z,a,s,tR,gl,glu);
@@ -31,10 +31,10 @@ public class Rabbit extends Critter{
 			drawArm(gl,glu);
 			gl.glDisable(GL2.GL_TEXTURE_2D);
 			
-			gl.glTranslatef(0, -BODY_RADIUS, 1.72f);
+			gl.glTranslatef(0, -BODY_RADIUS, .86f);
 			drawTail(gl,glu);
 
-			gl.glTranslatef(FOOT_CENTER_DISTANCE, -.76f, -3.09f);
+			gl.glTranslatef(FOOT_CENTER_DISTANCE, -.38f, -1.545f);
 			gl.glRotatef(32.86f,1,0,0);
 			gl.glEnable(GL2.GL_TEXTURE_2D);
 			drawFeet(gl,glu);
@@ -45,32 +45,31 @@ public class Rabbit extends Critter{
 	protected void drawBody(GL2 gl, GLU glu){
 		gl.glPushMatrix();
 			gl.glScalef(1, BODY_HEIGHT_WIDTH_RATIO, 1);
-			drawSphere(textureQuadric,BODY_RADIUS,glu);
+			drawTexturedUnitSphere(glu);
 		gl.glPopMatrix();
 	}
 	protected void drawHead(GL2 gl,GLU glu){
 		gl.glPushMatrix();
-			gl.glTranslatef(0, 2*BODY_HEIGHT_WIDTH_RATIO, -.6f);
+			gl.glTranslatef(0, BODY_HEIGHT_WIDTH_RATIO, -.3f);
 			gl.glEnable(GL2.GL_TEXTURE_2D);
-			drawSphere(textureQuadric,1.3f,glu);
+			drawSphere(textureQuadric,.65f,glu);
 			gl.glDisable(GL2.GL_TEXTURE_2D);
 			
 			gl.glColor3f(0, 0, 0);
-			float eyeZdist=-.71f;
-			gl.glTranslatef(0, EYE_CENTER_HEIGHT, eyeZdist);
-			drawEyes(gl,glu,EYE_CENTER_DISTANCE,.37f);
+			gl.glTranslatef(0, EYE_CENTER_HEIGHT, EYE_ZDIST);
+			drawEyes(gl,glu,.23f,.185f);
 			
-			gl.glTranslatef(0, -EYE_CENTER_HEIGHT, -1.3f-eyeZdist);
+			gl.glTranslatef(0, -EYE_CENTER_HEIGHT, -.65f-EYE_ZDIST);
 			drawNose(gl,glu);
 			// draw ears
-			gl.glTranslatef(0, .67f, 1.6f);
+			gl.glTranslatef(0, .335f, .8f);
 			drawEars(gl,glu);
 		gl.glPopMatrix();
 	}
 	protected void drawNose(GL2 gl,GLU glu){
 		// draw nose
 		gl.glColor3f(1, .77f, 1);
-		drawSphere(quadric, .26f, glu);
+		drawSphere(quadric, .13f, glu);
 		// draw whiskers
 		gl.glColor3f(WHISKER_COLOR,WHISKER_COLOR,WHISKER_COLOR);
 		gl.glBegin(GL2.GL_LINES);
@@ -86,8 +85,8 @@ public class Rabbit extends Critter{
 		int rotate=(int)(32f*Math.cos(Math.toRadians(t*360)))-10;
 		gl.glPushMatrix();
 			gl.glRotatef(rotate, 0, 0, -1);
-			gl.glTranslatef(2.55f,0,0);
-			gl.glScalef(3, .2f, .4f);
+			gl.glTranslatef(1.285f,0,0);
+			gl.glScalef(1.5f, .1f, .2f);
 			drawTexturedUnitSphere(glu);
 		gl.glPopMatrix();
 	}
@@ -95,19 +94,19 @@ public class Rabbit extends Critter{
 		int rotate=(int)(8f*Math.sin(Math.toRadians(t*360-45)));
 		gl.glPushMatrix();
 			gl.glRotatef(rotate, 0, 0, -1);
-			gl.glTranslatef(1.39f,1.16f,0);
-			gl.glScalef(1.64f, .6f, .6f);
+			gl.glTranslatef(.695f,.58f,0);
+			gl.glScalef(.82f, .3f, .3f);
 			drawTexturedUnitSphere(glu);
 		gl.glPopMatrix();
 	}
 	protected void drawTail(GL2 gl, GLU glu) {
 		gl.glColor3f(1, 1, 1);
-		drawSphere(quadric, .92f, glu);
+		drawSphere(quadric, .46f, glu);
 	}
 	protected void drawFeet(GL2 gl,GLU glu){
 		gl.glPushMatrix();
 			float xR=.3f;
-			gl.glScalef(xR, xR*4.2f, xR*2);
+			gl.glScalef(xR*.5f, xR*2.1f, xR);
 			drawTexturedUnitSphere(glu);	// draw foot
 			gl.glTranslatef(-2/xR*FOOT_CENTER_DISTANCE,0,0);
 			drawTexturedUnitSphere(glu);	// draw foot
