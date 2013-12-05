b443fd1ec7054d53a92f16ff57d719732c8dd109
DEVIN <midgleyd@bc.edu.>
2013-12-05 12:50:21 -0500

    Player Class now playing Music.

--- src/Music/Player.java
+++ src/Music/Player.java
@@ -6,20 +6,11 @@ public class Player {
 	
 	public static void main(String args[]){
 		m	= new JOALSoundMan();
-		String sound1 = "Score 3.wav";
+		String sound1 = "Score 3";
 		m.load(sound1, true);
 		m.setPos(sound1, 0, 0, 1);
-		// or: m.load("fire",  0, 0, 1, true);
 		m.setListenerPos(0, 0);
 		m.play(sound1);
-		for (int i=0; i<120; i++) {
-    		try {
-                Thread.sleep(500);
-                m.turnListener(10); // spin the listener
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-		}
 		m.cleanUp();
 	}
 }
