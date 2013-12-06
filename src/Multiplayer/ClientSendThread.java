f01e5252323074f2f7f0adacb1b5f032fef97bc1
midgleyd <midgleyd@cslab02.bc.edu>
2013-12-06 16:10:01 -0500

    reset ip to local host

--- src/Multiplayer/ClientSendThread.java
+++ src/Multiplayer/ClientSendThread.java
@@ -13,7 +13,7 @@ import game.*;
 public class ClientSendThread implements PlayerMotionWatcher{
 	private Player client; 
 	public static final int PORT = 9189;  
-	public static final String HOST = "136.167.178.58";  
+	public static final String HOST = "localhost";  
 	private DatagramSocket socket;
 	private byte[] buf;
 
