68afa29ef6ae4441ebcb1ff9aad99e0d838d7061
justin <justin@localhost>
2013-12-07 14:58:58 -0500

    commit to be able to do a pull

--- src/Multiplayer/GameServer.java
+++ src/Multiplayer/GameServer.java
@@ -3,7 +3,7 @@ import java.net.*;
 
 public class GameServer { 
 	public static final int PORT = 9189;  
-	public static final String HOST = "localhost"; //will change 
+	//public static final String HOST = "localhost"; //will change 
 
 	private MulticastSocket mcastSocket;
 	private int MCAST_PORT = 20000;
