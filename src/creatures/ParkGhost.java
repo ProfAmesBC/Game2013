a18168142aaac99709bae484848a84c010f5303b
James <parkuf@bc.edu>
2013-12-09 17:44:29 -0500

    I finished most of my ghost creature in a separate java project. Now attempting to transfer my ghost into the main game project.

---
+++ src/creatures/ParkGhost.java
@@ -0,0 +1,35 @@
+package creatures;
+
+import javax.media.opengl.GL2;
+import javax.media.opengl.glu.GLU;
+
+import game.PlayerMotionWatcher;
+
+public class ParkGhost implements Creature, PlayerMotionWatcher, ProjectileWatcher {
+
+	@Override
+	public void projectileMoved(double x, double z) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void playerMoved(float x, float y, float z, float angle,
+			float y_angle) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw(GL2 gl, GLU glu) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
