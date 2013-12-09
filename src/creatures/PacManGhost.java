7e50b9c275d98b519eebd3e58e16613a41b27225
James <parkuf@bc.edu>
2013-12-09 18:12:43 -0500

    Made a new branch, older version didn't seem to work. Again, trying to push my .java file into the master game project.

---
+++ src/creatures/PacManGhost.java
@@ -0,0 +1,39 @@
+package creatures;
+
+import javax.media.opengl.GL2;
+import javax.media.opengl.glu.GLU;
+import game.PlayerMotionWatcher;
+
+
+public class PacManGhost implements Creature, PlayerMotionWatcher, ProjectileWatcher {
+	
+	public PacManGhost() {
+		
+	}
+
+	@Override
+	public void projectileMoved(double x, double z) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void playerMoved(float x, float y, float z, float angle,
+			float y_angle) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw(GL2 gl, GLU glu) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
