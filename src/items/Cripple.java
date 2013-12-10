4a1118d4278e99e9b4cca0f0139b67e823f422ef
Francisco <Francisco@10.0.0.13>
2013-12-10 13:38:53 -0500

    tried to add a respawn counter for items.

--- src/items/Cripple.java
+++ src/items/Cripple.java
@@ -26,6 +26,7 @@ public class Cripple extends AbstractPowerUp {
 	private Bag bag;
 	private static PlayerAttributes p;
 	private int frames; 
+	private int respawn = 500;
 	private int counter;
 	private boolean used;	
 	private TextRenderer renderer;
@@ -50,6 +51,7 @@ public class Cripple extends AbstractPowerUp {
 	}
 
 	public void draw(GL2 gl, GLU glu) {
+		int timer = 0;
 		counter++;
 		frames++;
 		T = T + 0.5;
@@ -59,11 +61,12 @@ public class Cripple extends AbstractPowerUp {
 		}
 		if (!grabbed) {
 			drawItem(gl, glu);
-		}
-		else if(!used){			
+		}		
+		else if(!used){	
+			timer = frames; 			
 			use();
 			used=true;
-		}					
+		}		
 	}
 
 	private boolean grabConditions() {
@@ -269,7 +272,7 @@ public class Cripple extends AbstractPowerUp {
 	@Override
 	public void activate() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
