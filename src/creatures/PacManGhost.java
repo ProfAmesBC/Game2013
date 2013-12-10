1ed3dc98915f6f1812fd19cc35a034b1e33af67d
James <parkuf@bc.edu>
2013-12-10 13:21:50 -0500

    Transferred all code from personal java project to branch.

--- src/creatures/PacManGhost.java
+++ src/creatures/PacManGhost.java
@@ -1,11 +1,15 @@
 package creatures;
 
 import game.Building;
+
 import java.util.Random;
+
 import javax.media.opengl.GL2;
 import javax.media.opengl.glu.GLU;
 import javax.media.opengl.glu.GLUquadric;
+
 import com.jogamp.opengl.util.texture.Texture;
+
 import game.PlayerMotionWatcher;
 import game.PlayerStats;
 
@@ -32,7 +36,9 @@ public class PacManGhost implements Creature, PlayerMotionWatcher, ProjectileWat
 	private Random random = new Random();
 	private double k = random.nextDouble();
 	
-	public PacManGhost(GL2 gl, GLU glu) {
+	public PacManGhost(float x, float z, GL2 gl, GLU glu) {
+		X = x;
+		Z = z;
 		parkGhost = Building.setupTexture(gl, "ParkGhost.jpg");
 		parkGhostColor = Building.setupTexture(gl, "ParkGhostColor.jpg");
 		quadric = glu.gluNewQuadric();
@@ -40,22 +46,190 @@ public class PacManGhost implements Creature, PlayerMotionWatcher, ProjectileWat
         glu.gluQuadricNormals  (quadric, GLU.GLU_NONE); 	// GLU_NONE, GLU_FLAT, or GLU_SMOOTH
         glu.gluQuadricTexture  (quadric, true);        		// false, or true to generate texture coordinates
 	}
-
-	@Override
-	public void projectileMoved(double x, double z) {
-		// TODO Auto-generated method stub
-		
+	
+	public void drawTail(GL2 gl, GLU glu, float x, float y, float z) {
+		gl.glPushMatrix();
+			gl.glTranslatef(x, y, z);
+			float angle = (float) (10*Math.cos(Math.toRadians(T*360)));
+			gl.glRotatef(angle, 0, 0, 1);
+			gl.glRotatef(-90f, 1f, 0f, 0f); // stand upright (Y)
+			gl.glTranslatef(0, 0, -0.75f);
+			glu.gluCylinder(quadric, 0, 0.3, 0.75, 20, 20);
+		gl.glPopMatrix();
 	}
-
-	@Override
+	
+	public void drawTail2(GL2 gl, GLU glu, float x, float y, float z) {
+		gl.glPushMatrix();
+			gl.glTranslatef(x, y, z);
+			float angle = (float) (10*Math.sin(Math.toRadians(T*360-90))); 
+			gl.glRotatef(angle, 0, 0, 1);
+			gl.glRotatef(-90f, 1f, 0f, 0f); // stand upright (Y)
+			gl.glTranslatef(0, 0, -0.75f);
+			glu.gluCylinder(quadric, 0, 0.3, 0.75, 20, 20);
+		gl.glPopMatrix();
+	}
+	
+	public void drawOuterCircle(GL2 gl, GLU glu, float x, float y, float z) {
+		gl.glPushMatrix();
+			gl.glTranslatef(x, y, z);
+			gl.glRotatef(directionAngle, 0f, 1f, 0f);
+			drawTail(gl, glu, 0.7f, 1f, 0);
+			drawTail(gl, glu, -0.7f, 1f, 0);
+			drawTail(gl,glu, 0, 1f, 0.7f);
+			drawTail(gl,glu, 0, 1f, -0.7f);
+			drawTail(gl,glu, 0.494f, 1f, 0.494f);
+			drawTail(gl,glu, -0.494f, 1f, -0.494f);
+			drawTail(gl,glu, 0.494f, 1f, -0.494f);
+			drawTail(gl,glu, -0.494f, 1f, 0.494f);
+		gl.glPopMatrix();
+	}
+	
+	public void drawInnerCircle(GL2 gl, GLU glu, float x, float y, float z) {
+		gl.glPushMatrix();
+			gl.glTranslatef(x, y, z);
+			gl.glRotatef(directionAngle, 0f, 1f, 0f);
+			drawTail2(gl, glu, 0.4f, 1f, 0);
+			drawTail2(gl, glu, -0.4f, 1f, 0);
+			drawTail2(gl,glu, 0, 1f, 0.4f);
+			drawTail2(gl,glu, 0, 1f, -0.4f);
+			drawTail2(gl,glu, 0.282f, 1f, 0.282f);
+			drawTail2(gl,glu, -0.282f, 1f, -0.282f);
+			drawTail2(gl,glu, 0.282f, 1f, -0.282f);
+			drawTail2(gl,glu, -0.282f, 1f, 0.282f);
+		gl.glPopMatrix();
+	}
+	
+	public void drawAllTails (GL2 gl, GLU glu, float x, float y, float z) {
+		gl.glEnable(GL2.GL_TEXTURE_2D);
+		parkGhostColor.bind(gl);;
+		gl.glPushMatrix();
+			drawOuterCircle(gl,glu, x, y, z);
+			drawInnerCircle(gl,glu, x, y, z);
+		gl.glPopMatrix();
+		gl.glDisable(GL2.GL_TEXTURE_2D);
+	}
+	
+	public void drawBody(GL2 gl, GLU glu, float x, float y, float z) {
+		gl.glEnable(GL2.GL_TEXTURE_2D);
+		parkGhostColor.bind(gl);
+		gl.glPushMatrix();
+			gl.glTranslatef(x, y + 2.25f, z);
+			glu.gluSphere(quadric, 1, 40, 40);
+		gl.glPopMatrix();
+		
+        parkGhost.bind(gl);
+		gl.glPushMatrix();
+			gl.glTranslatef(x, y + 1f, z);
+			gl.glRotatef(directionAngle, 0f, 1f, 0f);
+			gl.glRotatef(-90f, 1f, 0f, 0f); // stand upright (Y)
+			glu.gluCylinder(quadric, 1, 1, 1.25, 40, 40);
+		gl.glPopMatrix();
+		gl.glDisable(GL2.GL_TEXTURE_2D);
+	}
+	
+	public void drawGhost(GL2 gl, GLU glu, float x, float y, float z) {
+		if (visible) {
+			drawAllTails(gl,glu, x, y, z);
+			drawBody(gl, glu, x, y, z);
+		}
+	}
+	
+	public void checkT() {
+		if (T > 300) {
+			T = 0f;
+		}
+	}
+	
 	public void draw(GL2 gl, GLU glu) {
-		// TODO Auto-generated method stub
+		drawGhost(gl, glu, X, Y, Z);
+		move();
+	}
+	
+	public void drawStillMotion() {
+		T = T + 0.99f;
+		checkT();
+		hoverHeight();
+	}
+	
+	public void hoverHeight() {
+		if (count < 50) {
+			Y = Y + 0.015f;
+			count = count + 1;
+		} else if (count < 100) {
+			Y = Y - 0.015f;
+			count = count + 1;
+		} if (count == 100) {
+			count = 0;
+			k = random.nextDouble();
+		}
+	}
+	
+	public void move() {
+		if (seesPlayer) {
+//			moveTowardsPlayer();
+		} else {
+			moveIdle();
+			drawStillMotion();
+		}
+	}
+	
+	public void playerMoved(float x, float y, float z, float angle, float yAngle) {
+		playerX = x;
+		playerZ = z;
+		playerAngle = angle;
+		double distance = Math.sqrt(Math.pow(X-x,2) + Math.pow(Z-z,2));
+		if(distance <= detectionRadius){
+			seesPlayer = true;
+		}
+		else{
+			seesPlayer = false;
+		}
+	}
+	
+	public void projectileMoved(double x, double z) {
+		if ( x > X - 1 && x < X + 1) {
+			if ( z > Z - 1 && z < Z + 1) {
+				shotByBullet = true;
+			}
+		}
 		
 	}
-
-	@Override
-	public void move() {
+	
+	public void moveTowardsPlayer(GL2 gl, GLU glu) {
+		directionAngle = playerAngle - 180;
 		
+		//get player x get player z
+		//get ghost x get ghost z
+		// find slope 
+		drawStillMotion();
+	}
+	
+	public void animateDeath(GL2 gl, GLU glu) {
+		gl.glColor3d(0,0,1);
+		if (Y <= 0) {
+			gl.glColor3d(1,1,1);
+			visible = false;
+		} else {
+			Y = Y - 0.03f;
+		}
+	}
+	
+	public void moveIdle() {
+		if (!seesPlayer) {
+			if (k <= 0.25) { 					//go left, directionAngle = 90
+				directionAngle = 90;
+				X = X + 0.05f;
+			} else if (k <= 0.5) { 				//go up, directionAngle = 0
+				directionAngle = 0;
+				Z = Z + 0.05f;
+			} else if (k <= 0.75) { 			//go right, directionAngle = 270
+				directionAngle = 270;
+				X = X - 0.05f;
+			} else { 							//go down, directionAngle = 180
+				directionAngle = 180;
+				Z = Z - 0.05f;
+			}
+		}
 	}
 
 	@Override
