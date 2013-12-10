bfbbf793d614a1147ac060c9dbfb41d950d92cee
Muhan <muhanz@gmail.com>
2013-12-10 16:25:22 -0500

    THANK YOU KOREA, PSY WILL LIVE ON FOREVER

--- src/game/Foreigner.java
+++ src/game/Foreigner.java
@@ -9,8 +9,11 @@ public class Foreigner extends Player{
 	private GL2 gl;
 	private GLU glu;
 	private Avatar psy;
+
+	private int waeyo = 0;
+	private boolean eotteohke;
 	//int id;
-	
+
 	@Override 
 	public float getX() {
 		return x;
@@ -52,44 +55,54 @@ public class Foreigner extends Player{
 	}
 
 	public Foreigner(GL2 gl, GLU glu, Integer nid) {
-		
+
 		//this.gl = gl;
 		//this.glu = glu;
-		
+
 		//random color
-			id = nid;
-			/*
+		id = nid;
+		/*
 			r = (float)Math.random(); //I would like to make these contingent on a hash of the player's username - Tyler
 			g = (float)Math.random(); 
 			b = (float)Math.random(); 
-			*/
-			
-	
-		
+		 */
+
+		eotteohke = true;
+
 	} 
-	
+
 	public void playerBody(GL2 gl, GLU glu, GLUquadric quadric, float x, float y, float z, float theta, float r, float g, float b, double size){
 		psy = new Avatar(gl,glu);
 		gl.glPushMatrix();
-			gl.glTranslatef(x, 0, z); // start position 
-			gl.glRotatef(theta, 0, 1, 0);
-			gl.glColor3f(r, g, b); //color
-			psy.draw(gl, glu);
-			
+		gl.glTranslatef(x, 0, z); // start position 
+		gl.glRotatef(theta, 0, 1, 0);
+		
+		if (eotteohke) {gl.glScaled(1,1,-1);}
+		
+		waeyo++;
+
+		if (waeyo > 10) {
+			waeyo = 0;
+			eotteohke = !eotteohke;
+		}
+		
+		gl.glColor3f(r, g, b); //color
+		psy.draw(gl, glu);
+
 		//	glu.gluSphere(quadric, size, 10, 10); //sphere for body 
 		gl.glPopMatrix();
-		
-//		gl.glPushMatrix();
-//			gl.glTranslatef(0, 0, 10);
-//			gl.glRotatef((float)theta, 0, 1, 0); //angle manipulation
-//		gl.glPopMatrix(); 
+
+		//		gl.glPushMatrix();
+		//			gl.glTranslatef(0, 0, 10);
+		//			gl.glRotatef((float)theta, 0, 1, 0); //angle manipulation
+		//		gl.glPopMatrix(); 
 
 	}
-	
+
 	public void draw(GL2 gl, GLU glu) {
 		playerBody(gl, glu, quadric, x,y,z,angle,r,g,b,10);
-	//	a =  new Avatar(gl, glu,x,y,z);
-	//	a.draw(gl, glu);
+		//	a =  new Avatar(gl, glu,x,y,z);
+		//	a.draw(gl, glu);
 	}
 
 }
