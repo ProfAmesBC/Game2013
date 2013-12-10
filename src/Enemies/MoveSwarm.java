97233ee4fa9fb420577e5b96c87e70c7f141dc29
Wesley <Wesley@localhost>
2013-12-10 11:53:51 -0500

    added second swarm, not tested yet. fixed direction of bats

--- src/Enemies/MoveSwarm.java
+++ src/Enemies/MoveSwarm.java
@@ -7,32 +7,43 @@ import javax.media.opengl.glu.GLU;
 
 public class MoveSwarm {
 	private List<BasicBat> swarm;
+	private List<BasicBat> swarm2;
 
 	public MoveSwarm(GL2 gl, GLU glu) {
 		swarm = new ArrayList<BasicBat>();
+		swarm2 = new ArrayList<BasicBat>();
 		
 		for (int i = 0; i < 50; i++) {
 			int j = (int) (Math.random()*20) - 10;
 			int k = (int) (Math.random()*20) - 10;
 			swarm.add(new BasicBat(gl, glu, 10+j, 50+k));
 		} 
-	}
-
-	public MoveSwarm(List<BasicBat> swarm){
-		this.swarm = swarm;
+		for (int i = 0; i < 50; i++) {
+			int j = (int) (Math.random()*20) - 10;
+			int k = (int) (Math.random()*20) - 10;
+			swarm2.add(new BasicBat(gl, glu, 200+j, 300+k));
+		}
 	}
 
 	public void draw(GL2 gl, GLU glu) {
 		
 		for (BasicBat b: swarm) {
-			b.draw2(gl, glu);
-			if (b.isDead())
-				swarm.remove(b);
-			else {
-				b.setX(b.getX()+b.getDX());
-				b.setZ(b.getZ()+b.getDZ());
+			for (BasicBat c: swarm2) {
+				c.draw2(gl, glu);
+				b.draw2(gl, glu);
+				if (b.isDead())
+					swarm.remove(b);
+				if (c.isDead())
+					swarm2.remove(c);
+				else {
+					b.setX(b.getX()+b.getDX());
+					b.setZ(b.getZ()+b.getDZ());
+					c.setX(c.getX()+c.getDX());
+					c.setZ(c.getZ()+c.getDZ());
+				}
 			}
 		}
+		
 		
 		for (BasicBat b: swarm) {
 			for (BasicBat t: swarm) {
@@ -54,6 +65,29 @@ public class MoveSwarm {
 						b.setDirection(0);
 					else	
 						b.setDirection((float) Math.toDegrees(Math.atan2(-b.getDZ(), b.getDX())));
+				}				
+			}
+		}
+		for (BasicBat m: swarm2) {
+			for (BasicBat n: swarm2) {
+				if(m != n) {
+					//calc distances
+					float xdist = n.getX()-m.getX();
+					float zdist = n.getZ()-m.getZ();
+
+					//calc force
+					float distancesq = (xdist*xdist + zdist*zdist);
+					if (distancesq < 0.1f)
+						distancesq = 0.1f;
+					float force = .001f/distancesq;
+
+					//update velocity vectors
+					m.setDX(m.getDX()+(force*xdist));
+					m.setDZ(m.getDZ()+(force*zdist));
+					if (m.getDZ() == 0 && m.getDX() == 0) 
+						m.setDirection(0);
+					else	
+						m.setDirection((float) Math.toDegrees(Math.atan2(-m.getDZ(), m.getDX())));
 				}				
 			}
 		}
