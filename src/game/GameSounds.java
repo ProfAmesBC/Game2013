82e5c4b0e7690108d52d62d5317cad93d3f99333
Vince <mendolav@bc.edu>
2013-12-10 19:05:49 -0500

    Real sound test

--- src/game/GameSounds.java
+++ src/game/GameSounds.java
@@ -1,9 +1,62 @@
 package game;
 
+
 public class GameSounds {
+
+	private static GameSoundMan m=null;
+	
+//		m	= new GameSoundMan();
+//		String sound1 = "fire";
+//		m.load(sound1, true);
+//		m.setPos(sound1, 0, 0, 1);
+//		// or: m.load("fire",  0, 0, 1, true);
+//		m.setListenerPos(0, 0);
+//		m.play(sound1);
+//		for (int i=0; i<120; i++) {
+//    		try {
+//                Thread.sleep(500);
+//                m.turnListener(10); // spin the listener
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//		}
+//		m.cleanUp();
+
 
 	public static void footstepNoise()
 	{
+		
+//		String sound1 = "fire";
+//		m.load(sound1, true);
+//		m.setPos(sound1, 0, 0, 1);
+//		// or: m.load("fire",  0, 0, 1, true);
+//		m.setListenerPos(0, 0);
+//		m.play(sound1);
+//
+//		m.cleanUp();
+		
+		
+		m	= new GameSoundMan();
+		String sound1 = "footsteps-5 edited";
+		m.load(sound1, true);
+		m.setPos(sound1, 0, 0, 1);
+		// or: m.load("fire",  0, 0, 1, true);
+		m.setListenerPos(0, 0);
+		m.play(sound1);
+		for (int i=0; i<120; i++) {
+    		try {
+                Thread.sleep(500);
+                m.turnListener(10); // spin the listener
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+		}
+		m.cleanUp();
+
+		
+		
+		
+		
 		System.out.println("Footstep");
 	}
 	
